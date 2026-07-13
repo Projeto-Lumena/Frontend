34241ddbb4f9dc2b6535d8b7f1b97e049b22b78e
AmandaRibeiroLuz <amanda.ribeiro.da.luz.201@gmail.com>
2026-07-13 16:45:13 -0300

    FEAT: Início do desenvolvimento da página do Produto. #33

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-3f1afc6c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m0v6167pe6c"
+    "revision": "0.r1pco175ea4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
