301bd32b1b4bc1d53221faba0a4cad872d8b706b
AmandaRibeiroLuz <amanda.ribeiro.da.luz.201@gmail.com>
2026-08-17 15:11:22 -0300

    FEAT: Alteração no header mobile. #30

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -22,56 +22,67 @@ function handleLogout() {
 }
 </script>
 <template>
-    <header class="fixed z-50 top-0 left-0 w-full bg-[#0C2645] p-8">
+    <header class="hidden md:block fixed z-50 top-0 left-0 w-full bg-[#0C2645] p-8">
         <ul class="flex gap-5 items-center justify-between sm:gap-20 md:gap-10">
             <li>
                 <ul class="flex items-center gap-5">
-                    <li class="md:hidden">
-                        <button @click="abrirMenu">
-                            <img src="/icons/menu-hamburger.svg" alt="Menu">
-                        </button>
-                    </li>
                     <li class="w-20 mb-2 xl:mr-30 lg:ml-8">
-                        <router-link to="/">
+                        <RouterLink to="/">
                             <img src="/img/logo.svg" alt="Lumena">
-                        </router-link>
+                        </RouterLink>
                     </li>
                 </ul>
             </li>
             <li>
                 <ul class="flex items-center gap-5 xl:gap-15 xl:mr-10">
                     <li
-                        class="text-white border-b flex w-20 pb-1 gap-5 font-sen md:w-50 md:gap-30 xl:w-80  justify-between 2xl:gap-60">
-                        <p>Buscar</p> <img src="/icons/procurar.svg" alt="Procura">
+                        class="text-white border-b flex w-20 pb-1 gap-5 font-sen md:w-50 md:gap-30 xl:w-80 justify-between 2xl:gap-60">
+                        <p>Buscar</p>
+                        <img src="/icons/procurar.svg" alt="Procurar">
                     </li>
                     <li>
                         <RouterLink to="/perfil">
-                            <img class="w-6 md:w-8" src="/icons/usuario.svg" alt="Perfil ">
+                            <img class="w-6 md:w-8" src="/icons/usuario.svg" alt="Perfil">
                         </RouterLink>
                     </li>
                     <li>
-                        <img class="w-6 md:w-8" src="/icons/sacola.svg" alt="Sacola">
+                        <RouterLink to="/sacola">
+                            <img class="w-6 md:w-8" src="/icons/sacola.svg" alt="Sacola">
+                        </RouterLink>
                     </li>
                     <button v-if="authStore.isAuthenticated" @click="handleLogout"
                         class="flex flex-col items-center justify-center text-white font-sen w-6 md:w-8">
-                        <img class="w-6 md:w-8 text-white" src="/icons/user-logout-white.svg" alt="Logout">
+                        <img class="w-6 md:w-8" src="/icons/user-logout-white.svg" alt="Logout">
                         <span class="text-xs md:text-sm">Sair</span>
                     </button>
                 </ul>
             </li>
         </ul>
-        <div v-if="menuAberto"
-            class="fixed top-24 -z-10 left-0 w-1/2 h-full bg-[#0C2645] text-[#F8F9F9] shadow-lg p-6 flex flex-col gap-80">
-            <div class="text-[#F8F9F9] flex flex-col gap-6 ">
-                <router-link to="/" @click="fecharMenu"
-                    class="p-2 font-sen hover:font-bold router-link-active:font-bold">Início</router-link>
-                <router-link to="/produtos" @click="fecharMenu"
-                    class="p-2 font-sen hover:font-bold router-link-active:font-bold">Produtos</router-link>
-                <router-link :to="authStore.isAuthenticated ? '/perfil' : '/login'" @click="fecharMenu"
-                    class="p-2 font-sen hover:font-bold router-link-active:font-bold">Perfil</router-link>
-                <router-link to="/sacola" @click="fecharMenu"
-                    class="p-2 font-sen hover:font-bold router-link-active:font-bold">Sacola</router-link>
-            </div>
-        </div>
     </header>
+    <div class="md:hidden fixed bottom-0 left-0 z-50 w-full h-20 bg-[#0C2645] shadow-[0_-4px_15px_rgba(0,0,0,0.15)]">
+        <ul class="h-full flex items-center justify-around">
+            <li class="h-full flex-1">
+                <RouterLink to="/" class="h-full flex flex-col items-center justify-center gap-1 text-white"
+                    active-class="text-[#FDA202]">
+                    <img src="/icons/home.svg" alt="Home" class="w-6 h-6">
+                    <span class="text-sm font-sen">Home</span>
+                </RouterLink>
+            </li>
+            <li class="h-full flex-1">
+                <RouterLink :to="authStore.isAuthenticated ? '/perfil' : '/login'"
+                    class="h-full flex flex-col items-center justify-center gap-1 text-white"
+                    active-class="text-[#FDA202]">
+                    <img src="/icons/usuario.svg" alt="Perfil" class="w-6 h-6">
+                    <span class="text-sm font-sen"> Perfil</span>
+                </RouterLink>
+            </li>
+            <li class="h-full flex-1">
+                <RouterLink to="/sacola" class="h-full flex flex-col items-center justify-center gap-1 text-white"
+                    active-class="text-[#FDA202]">
+                    <img src="/icons/sacola.svg" alt="Sacola" class="w-6 h-6">
+                    <span class="text-sm font-sen">Sacola </span>
+                </RouterLink>
+            </li>
+        </ul>
+    </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -137,11 +137,7 @@ const productsFiltradas = computed(() => {
   <Banner :imagens="store.getBanners('home')" />
 
   <Transition name="fade">
-    <div v-if="showLoginMessage" class="fixed top-30 left-1/2 -translate-x-1/2 z-50
-           flex items-center gap-2
-           rounded-lg border border-green-200
-           bg-green-50 px-6 py-3
-           text-green-700 shadow-lg">
+    <div v-if="showLoginMessage" class="fixed top-30 left-1/2 -translate-x-1/2 z-50 flex items-center gap-2 rounded-lg border border-green-200 bg-green-50 px-6 py-3 text-green-700 shadow-lg">
       <span class="text-lg">✓</span>
       <span>Login realizado com sucesso! Seja bem-vindo(a).</span>
     </div>
